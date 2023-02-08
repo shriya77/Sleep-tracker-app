4ab326b9ab22b75ed3eb49d0a3ce14d180c640d7
Shriya Kalyan <shriyakkx@gmail.com>
2023-02-08 08:50:42 +0800

    Update contentview.swift

--- contentview.swift
+++ contentview.swift
@@ -6,6 +6,16 @@ struct ContentView: View {
     @State private var outputValue = ""
     
     @State private var inputUnitValue = 2
+    let inputUnits = ["metres", "kilometres", "feet", "miles"]
+    
+    @State private var outputUnitValue = 2
+    let outputUnits = ["metres", "kilometres", "feet", "miles"]
+    
+    var inputUnitAfterConversionToOutput: String {
+        
+        var output = ""
+        
+    }
     
     var body: some View {
         Text("Hello, World!").padding()
